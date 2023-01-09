22d1e85601668108c063eb181d58502f07d9fafc
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2023-01-10 02:35:51 +0900

    Time: 23 ms (9.09%), Space: 42.8 MB (69.58%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        
+        map.put("IV", 4);
+        map.put("IX", 9);
+        map.put("XL", 40);
+        map.put("XC", 90);
+        map.put("CD", 400);
+        map.put("CM", 900);
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
+        
+        List<String> list = Arrays.stream(s.split("")).map(e -> e).collect(Collectors.toList());
+        
+        int answer = 0;
+        // int limit = list.size();
+        for (int i=0; i<list.size()-1; i++) {
+            String temp = list.get(i) + list.get(i+1);
+            
+            if (map.containsKey(temp)) {
+                answer += map.get(temp);
+                list.remove(i);
+                list.remove(i);
+                
+                i -= 1;
+                // limit -= 2;
+            }
+        }
+        
+        for (String str : list) {
+            answer += map.get(str);
+        }
+        
+        return answer;
+    }
+}
