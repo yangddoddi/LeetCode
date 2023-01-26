1136c5d64bbe8c64852c4754896ea151b4f0143f
yang eun chan <97802103+yangddoddi@users.noreply.github.com>
2023-01-26 09:20:48 +0900

    Time: 830 ms (13.61%), Space: 146.3 MB (5.11%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        
+        for (int i=0; i<nums.length-1; i++) {
+            int j = i+1;
+            int k = nums.length-1;
+                
+            while (j < k) {
+                int n = nums[i] + nums[j] + nums[k];
+                
+                if (n > 0) {
+                    k--;
+                }
+                
+                if (n < 0) {
+                    j++;
+                }
+                
+                if (n == 0) {
+                    List<Integer> list = List.of(nums[i], nums[j], nums[k]);
+                    set.add(list);
+                    k--;
+                }
+            }
+        }
+        
+        return new ArrayList<>(set);
+    }
+}
